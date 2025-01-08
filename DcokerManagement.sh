24013a5ac693fc4f586a8db08fe3ace18fc14de5
ASHUTOSH KUMAR PRASAD <114760574+ap0267@users.noreply.github.com>
2025-01-08 15:51:45 +0530

    Update DcokerManagement.sh

--- DcokerManagement.sh
+++ DcokerManagement.sh
@@ -13,7 +13,7 @@ while true; do
             echo "9. Delete a Container"
 	    echo "10. Delete an Image"
             echo "11. Exit"
-							    							        read -p "Enter your choice: " choice
+	    read -p "Enter your choice: " choice
 
 	    if [ "$choice" -eq 1 ]; then
 		read -p "Enter the image name to pull (e.g., ubuntu:latest): " image_name
@@ -24,37 +24,39 @@ while true; do
 		 read -p "Enter the image name to run in interactive mode: " image_name
 		 docker run -it "$image_name"
 
-															elif [ "$choice" -eq 3 ]; then
-		     read -p "Enter the container name or ID to start: " container_id			       		         docker start "$container_id"
-		     echo "Container '$container_id' started successfully."                                      	
+	    elif [ "$choice" -eq 3 ]; then  
+		     read -p "Enter the container name or ID to start: " container_id
+       		     docker start "$container_id"
+		     echo "Container '$container_id' started successfully."      
+       
 	    elif [ "$choice" -eq 4 ]; then
 		    read -p "Enter the container name or ID to attach: " container_id
 		    docker attach "$container_id"
 
-															elif [ "$choice" -eq 5 ]; then
+	     elif [ "$choice" -eq 5 ]; then
 		    read -p "Enter the container name or ID to stop: " container_id
 		    docker stop "$container_id"
 		    echo "Container '$container_id' stopped successfully."
-																												    elif [ "$choice" -eq 6 ]; then
-												
-		    echo "\nAll Containers:"									         	docker ps -a                                                                      															        
+		    echo "\nAll Containers:"
+      		    docker ps -a    
+	    
 	    elif [ "$choice" -eq 7 ]; then
-		    echo "\nAll Images:"											docker images
-
-																												    elif [ "$choice" -eq 8 ]; then
+		    echo "\nAll Images:"
+      		    docker images																									    elif [ "$choice" -eq 8 ]; then
 		    echo "\nRunning Containers:"
-		    docker ps																																				        
+		    docker ps	
+      
 	    elif [ "$choice" -eq 9 ]; then
 		    read -p "Enter the container name or ID to delete: " container_id
 		    docker rm "$container_id"
 		    echo "Container '$container_id' deleted successfully."
 
-															 elif [ "$choice" -eq 10 ]; then
+	    elif [ "$choice" -eq 10 ]; then
 		    read -p "Enter the image name or ID to delete: " image_name
 		    docker rmi "$image_name"
 		    echo "Image '$image_name' deleted successfully."
 
-															elif [ "$choice" -eq 11 ]; then
+	    elif [ "$choice" -eq 11 ]; then
 		    echo " Goodbye!....see you soon...see you very soon"
 		    break
 
